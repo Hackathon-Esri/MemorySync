aedb03379940adf4d4ce3732d50940a7e38c93af
fcamas <fredycamasdev@gmail.com>
2024-07-26 19:22:45 -0700

    Initial Commit

---
+++ Chrononauts/ChrononautsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChrononautsApp.swift
+//  Chrononauts
+//
+//  Created by FredyCamas on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ChrononautsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
